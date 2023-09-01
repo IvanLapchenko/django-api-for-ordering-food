9d25e0f09a8caeb5039e1c36743701bad44654d7
Ivan Lapchenko <lapchenko.vanya@gmail.com>
2023-09-01 14:17:29 +0300

    create models and migrate

--- main/models.py
+++ main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=100)
+    order_type = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.order_number
+
+
+class Check(models.Model):
+    CLIENT = 'client'
+    KITCHEN = 'kitchen'
+    TYPE_CHOICES = [
+        (CLIENT, 'client'),
+        (KITCHEN, 'kitchen'),
+    ]
+
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    pdf_link = models.FileField(upload_to='media/pdf')
+    status = models.CharField(max_length=50)
+    type = models.CharField(max_length=8, choices=TYPE_CHOICES)
